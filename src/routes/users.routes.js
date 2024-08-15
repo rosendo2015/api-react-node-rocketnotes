d0d83a0e21b3c6b40b9139167ed780d150812bdf
rosendo <rosendc30@gmail.com>
2024-08-15 18:03:44 -0300

    middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,6 +2,8 @@ const {Router} = require("express");
 const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
 
+
+
 const usersController = new UsersController();
 usersRoutes.post("/", usersController.create);
 module.exports = usersRoutes;
