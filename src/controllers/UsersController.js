a2f739631ec0324b9deb7533a2098d7cd5d1257a
rosendo <rosendc30@gmail.com>
2024-08-17 22:13:39 -0300

    CREATE USER

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,15 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 class UsersController{
-create(request, response){
+async create(request, response){
     const {name, email, password} = request.body;
-    if(!name){
-        throw new AppError("Nome é obrigatório.")
+    const database = await sqliteConnection();
+    const checkExistsEmailUser = await database.get("SELECT * FROM users WHERE email = (?)",[email])
+    if(checkExistsEmailUser){
+        throw new AppError("Este email já está em uso.")
     }
-    response.status(201).json({name, email, password})
+    await database.run("INSERT INTO users(name, email, password) VALUES (?,?,?)",[name, email, password])
+    return response.status(201).json()
 }
 }
 module.exports = UsersController;
