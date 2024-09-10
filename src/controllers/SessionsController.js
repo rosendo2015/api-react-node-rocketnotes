9e030309a51092e74a915db1f07a712c72d80d44
rosendo <rosendc30@gmail.com>
2024-09-09 23:07:01 -0300

    controller sessions

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,14 @@
+const knex = require("../knex");
+
 class SessionsController{
- async create(){
-    
- }
+async create(request, response){
+    const {email, password} = request.body
+
+    const user = await knex
+
+
+    return response.json({email, password})
 }
+}
+
 module.exports = SessionsController;
