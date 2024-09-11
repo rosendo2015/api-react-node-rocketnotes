342686be40f50bc0263c884062a9a2052496cb72
rosendo <rosendc30@gmail.com>
2024-09-10 21:41:14 -0300

    JWT configs

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const { compare } = require("bcryptjs");
 const knex = require("../knex");
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken");
 
 class SessionsController{
 async create(request, response){
@@ -16,8 +18,14 @@ async create(request, response){
     if(!passwordMatched){
         throw new AppError("E-Mail e/ou Senha incorretos!", 401)
     }
+    const {secret, expiresIn } = authConfig.jwt;
+    const token = sign({},secret,{
+        subject: String(user.id),
+        expiresIn
+    })
 
-    return response.json(user)
+
+    return response.json({user, token})
 }
 }
 
