9fc3d765823bdf359e02826bc93e6c0c1a00802a
rosendo <rosendc30@gmail.com>
2024-09-10 21:32:35 -0300

    validação

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,13 +1,23 @@
+const { compare } = require("bcryptjs");
 const knex = require("../knex");
+const AppError = require("../utils/AppError")
 
 class SessionsController{
 async create(request, response){
     const {email, password} = request.body
 
-    const user = await knex
+    const user = await knex("users").where({email}).first();
 
+    if(!user){
+        throw new AppError("E-Mail e/ou Senha incorretos!", 401)
+    }
+    const passwordMatched = await compare(password, user.password)
+   
+    if(!passwordMatched){
+        throw new AppError("E-Mail e/ou Senha incorretos!", 401)
+    }
 
-    return response.json({email, password})
+    return response.json(user)
 }
 }
 
